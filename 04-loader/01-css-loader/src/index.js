1c258511fc8e42b34d6142d03faa95a346b87821
zhaoyifan <zhaoyifans@gmail.com>
2019-10-21 21:17:25 +0800

    feat: css loader

---
+++ 04-loader/01-css-loader/src/index.js
@@ -0,0 +1,7 @@
+import writeContent from './writeContent.js';
+import './style.css';
+
+
+document.write('My first Webpack app.<br/>');
+document.write("<p class='title'>same publicPath. </p><br/>");
+writeContent();
---
+++ 04-loader/README.md
@@ -0,0 +1,16 @@
+# 预处理器
+
+## loader
+
+用公式表达 loader 的本质：
+
+```
+output = loader(input)
+
+eg:
+ES5 = babel-loader(ES6+)
+
+style 标签 = style-loader(css-loader(sass-loader(SCSS)))
+```
+
+ 
