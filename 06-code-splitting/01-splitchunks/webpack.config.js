b9bbd70420848980d7e941c9aa49b279e51cda9c
zhaoyifan <zhaoyifans@gmail.com>
2019-11-01 15:36:11 +0800

    feat: code splitting and config in prod

---
+++ 06-code-splitting/01-splitchunks/webpack.config.js
@@ -0,0 +1,122 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const webpack = require('webpack'); // to access built-in plugins
+
+module.exports = {
+    context: path.join(__dirname, 'src'),
+    entry: {
+        app: './index.js',
+        // vendor: ['lodash'], webpack 4 已经不推荐使用
+    },
+    output: {
+        filename: '[name]@[chunkhash].js', // 输出资源的文件名
+        path: path.resolve(__dirname, 'dist'), //输出资源的位置，要求必须为绝对路径，wp4 默认为 dist
+        // publicPath: '/dist/', // 资源的请求位置 
+    },
+    mode: 'development',
+    devServer: {
+        // contentBase: path.join(__dirname, 'dist'), // 告诉服务器从哪个目录中提供内容  
+        // open: 'Google Chrome',
+        // port: 8080,
+        publicPath: '/', // 此路径下的打包文件可在浏览器中访问。
+    },
+
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin(),
+
+        // HtmlWebpackPlugin 创建入口 html
+        new HtmlWebpackPlugin({
+            title: path.basename(__dirname)
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+    ],
+    module: {
+        rules: [{
+            enforce: 'pre',
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'eslint-loader',
+        }, {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name(file) {
+                        if (process.env.NODE_ENV === 'development') {
+                            return '[path][name].[ext]';
+                        }
+                        return '[contenthash].[ext]';
+                    },
+                },
+            }, ],
+        }, {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    // https://babeljs.io/docs/en/babel-preset-env#modules
+                    presets: [
+                        [
+                            "@babel/preset-env",
+                            {
+                                "useBuiltIns": "entry",
+                                "corejs": 2
+                            }
+                        ]
+                    ]
+                }
+            }
+        }, {
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true,
+                    },
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true,
+                    },
+                }
+            ],
+        }, {
+            // 被加载模块是 resource
+            resource: {
+                test: /\.css$/,
+                exclude: /node_modules/, // 与 include 同时存在是优先级比 include 高
+                include: /src/,
+            },
+            use: [
+                'style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 1
+                    }
+                },
+                'postcss-loader'
+            ], // 倒序处理 
+        }]
+    }
+}
---
+++ 06-code-splitting/README.md
@@ -0,0 +1,7 @@
+# 代码分片
+
+## Webpack 4
+
+分离 app(应用程序) 和 vendor(第三方库) 入口
+
+> 在 webpack < 4 的版本中，通常将 vendor 作为单独的入口起点添加到 entry 选项中，以将其编译为单独的文件（与 CommonsChunkPlugin 结合使用）。而在 webpack 4 中不鼓励这样做。而是使用 optimization.splitChunks 选项，将 vendor 和 app(应用程序) 模块分开，并为其创建一个单独的文件。不要 为 vendor 或其他不是执行起点创建 entry。
